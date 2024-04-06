24c8a8b272a68742d30927b2e50ab073e92c2c83
YAKOUBA GOITA <143269581+YAKOUBAGOITA@users.noreply.github.com>
2024-04-06 12:20:55 +0530

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def register(request):
@@ -11,4 +12,11 @@ def register(request):
         password2=request.POST['password2']
         email=request.POST['email']
 
-    return render(request,'register.html')
+        user=User.objects.create_user(username=username, password=password1, email=email,
+         first_name=first_name, last_name=last_name)
+        user.save()
+        print('user created')
+        return redirect('/')
+    else:
+
+        return render(request,'register.html')
