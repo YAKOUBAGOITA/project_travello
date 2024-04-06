736c433f443c1655fecbe0cb30aee97f9844c1f3
YAKOUBA GOITA <143269581+YAKOUBAGOITA@users.noreply.github.com>
2024-04-06 11:45:31 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        username=request.POST['username']
+        password1=request.POST['password1']
+        password2=request.POST['password2']
+        email=request.POST['email']
+
+    return render(request,'register.html')
